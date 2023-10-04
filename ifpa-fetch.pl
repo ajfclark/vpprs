49f8a8112158355184fffb9b27f9972f2c18ef49
Andrew Clark <ajfclark@gmail.com>
2023-10-05 09:41:53 +1100

    Store data retrieved to allow manipulation

--- ifpa-fetch.pl
+++ ifpa-fetch.pl
@@ -4,7 +4,8 @@ import httplib2
 from bs4 import BeautifulSoup
 import argparse
 
-parser = argparse.ArgumentParser(description="Script to fetch tournament results and dump a CSV", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description="Script to fetch tournament results and dump a CSV",
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-t", "--tournament", required=True)
 args = parser.parse_args()
 config = vars(args)
@@ -17,9 +18,12 @@ soup = BeautifulSoup(response, 'html.parser')
 table = soup.find("table", id="tourresults").tbody
 rows = table.find_all("tr")
 
+data = []
 for row in rows:
 	td = row.find_all("td")
-	rank = td[0].contents[0]
+	place = td[0].contents[0]
 	name = td[1].a.contents[0]
+	data.append({ "place": place, "name": name })
 
-	print(rank + "," + name)
+for i in range(len(data)):
+	print(str(i) + ":" + str(data[i]))
