6be5f8418c751c8b20c88480588ed8c1ab9370a9
Andrew Clark <ajfclark@gmail.com>
2023-10-05 10:25:25 +1100

    Modified placings in line with desired result

--- ifpa-fetch.pl
+++ ifpa-fetch.pl
@@ -4,11 +4,11 @@ import httplib2
 from bs4 import BeautifulSoup
 import argparse
 
-parser = argparse.ArgumentParser(description="Script to fetch tournament results and dump a CSV",
+parser = argparse.ArgumentParser(description='Script to fetch tournament results and dump a CSV',
 	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-t", "--tournament")
-group.add_argument("-f", "--file")
+group.add_argument('-t', '--tournament')
+group.add_argument('-f', '--file')
 args = parser.parse_args()
 config = vars(args)
 
@@ -21,15 +21,28 @@ if(config['file']):
 
 soup = BeautifulSoup(htmlPage, 'html.parser') 
 
-table = soup.find("table", id="tourresults").tbody
-rows = table.find_all("tr")
+table = soup.find('table', id='tourresults').tbody
+rows = table.find_all('tr')
 
 data = []
 for row in rows:
-	td = row.find_all("td")
-	place = td[0].contents[0]
-	name = td[1].a.contents[0]
-	data.append({ "place": place, "name": name })
+	cells = row.find_all('td')
+	place = cells[0].contents[0].strip()
+	name = cells[1].a.contents[0].strip()
+	data.append({ 'placing': place, 'name': name })
 
 for i in range(len(data)):
-	print(str(i) + ":" + str(data[i]))
+	matches = [ i ]
+	for k in range(i + 1, len(data)):
+		if(data[i]['placing'] == data[k]['placing']):
+			matches.append(k)
+	if(len(matches) > 1):
+		total = 0
+		for k in range(len(matches)):
+			total = total + int(matches[k]) + 1
+		average = total / len(matches)
+		for k in range(len(matches)):
+			data[matches[k]]['placing'] = average
+
+for player in data:
+	print(str(player['placing']) + '	' + player['name'])
