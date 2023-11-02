9dec5676daf5044e8007b51403869562d78c3487
Andrew Clark <ajfclark@gmail.com>
2023-11-02 11:41:51 +1100

    Completed db update functionality in ifpa-results.py

--- cli/ifpa.py
+++ cli/ifpa.py
@@ -5,8 +5,19 @@ import requests
 def getCalendar(apikey, country):
     # Get the calendar of past events
     params = { 'api_key': apikey, 'country': country }
-    r = requests.get('https://api.ifpapinball.com/v1/calendar/history', params=params)
+    url = 'https://api.ifpapinball.com/v1/calendar/history'
+    r = requests.get(url, params=params)
     if r.status_code != 200:
         r.raise_for_status()
 
     return r.json()['calendar']
+
+def getTournamentResults(apikey, tournamentId):
+    params = { 'api_key': apikey }
+    url = 'https://api.ifpapinball.com/v1/tournament/' + tournamentId + '/results'
+
+    r = requests.get(url, params=params)
+    if r.status_code != 200:
+        r.raise_for_status()
+
+    return r.json()['tournament']
