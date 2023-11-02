ed7dea6c7531d9e37b8f84551b0ef2b38e646b18
Andrew Clark <ajfclark@gmail.com>
2023-11-02 11:55:51 +1100

    Increased resolution of standings

--- html/standings.php
+++ html/standings.php
@@ -19,7 +19,7 @@ print('<tr><th>Rank</th><th>Player</th><th>Events</th><th>Wins</th><th>Average</
 $i = 0;
 while($row = pg_fetch_row($ret)) {
 	$i++;
-	printf('<tr><td>%d</td><td>%s</td><td>%d</td><td>%d</td><td>%0.1f</td><td>%0.1f<td></tr>', $i, $row[0], $row[1], $row[2], $row[3], $row[4]);
+	printf('<tr><td>%d</td><td>%s</td><td>%d</td><td>%d</td><td>%0.2f</td><td>%0.2f<td></tr>', $i, $row[0], $row[1], $row[2], $row[3], $row[4]);
 }
 print("</table>\n");
 pg_close($db);
