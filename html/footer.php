29b46a7efcec8b5418764c084b8889564a309f20
Andrew Clark <ajfclark@gmail.com>
2023-11-05 17:34:20 +1100

    updated footer

--- html/footer.php
+++ html/footer.php
@@ -1,4 +1,4 @@
 <hr>
-<a href="top32.php">Top 32</a> <a href="standings.php">Standings</a> <a href="event.php">Event List</a>
+<a href="standings.php">Top 32</a> <a href="standings.php?full">Full Standings</a> <a href="event.php">Event List</a>
 </body>
 </html>
