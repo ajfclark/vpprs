cac3d6cb9259321b85a8b5c0a2025bf925944602
Andrew Clark <ajfclark@gmail.com>
2023-11-20 16:50:30 +1100

    Updated matchplay output

--- cli/vppr.py
+++ cli/vppr.py
@@ -4,9 +4,9 @@ import psycopg2
 
 def vppr(place: float, numPlayers: int) -> float:
     if(place == 1.0):
-        return str(50)
+        return float(50)
     else:
-        return str(((int(numPlayers) - float(place) + 1) / numPlayers)**2 * 45 + 1)
+        return float(((int(numPlayers) - float(place) + 1) / numPlayers)**2 * 45 + 1)
 
 def getPlayerId(cursor, name: str) -> int:
     cursor.execute("SELECT id FROM player WHERE name='" + str(name) + "';")
